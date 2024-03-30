74de042a05f08e71ce71b5ca7b1c56cfeeeb98bf
VedantLaptop <vedant.naik09@gmail.com>
2024-03-31 00:11:04 +0530

    Calendar Implemented

---
+++ frontend/src/constants/calendar/index.js
@@ -0,0 +1,20 @@
+export const user = {
+    "username": "JohnDoe",
+    "points": 250,
+    "weightHistory": [
+      {"date": "2024-02-01", "weight": 1.1},
+      {"date": "2024-02-10", "weight": 2.3},
+      {"date": "2024-02-20", "weight": 3.5},
+      {"date": "2024-02-28", "weight": 4.7},
+      {"date": "2024-03-07", "weight": 3.9},
+      {"date": "2024-03-15", "weight": 2.8},
+      {"date": "2024-03-22", "weight": 1.6},
+      {"date": "2024-03-06", "weight": 4.4},
+      {"date": "2024-03-14", "weight": 3.1},
+      {"date": "2024-03-25", "weight": 3.1},
+      {"date": "2024-03-26", "weight": 3.1},
+      {"date": "2024-03-27", "weight": 3.1},
+      {"date": "2024-03-28", "weight": 3.1},
+    ]
+  }
+  
